4dd2354bff54f14a60112a0cbceeb6fb49d3898a
Kavindu Theekshana <kavindutheekshana@gmail.com>
2024-12-31 15:55:30 +0000

    ID DESC

--- resources/views/backend/applications/index.blade.php
+++ resources/views/backend/applications/index.blade.php
@@ -210,7 +210,7 @@
     $(document).ready(function() {
         $('#example').DataTable({
             "order": [
-                [0, "desc"]
+                [1, "desc"]
             ],
             columnDefs: [{
                     width: "2%",
