3f65d835717c0fe598a96790eb2f80fe31d65c4d
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-01-29 19:43:37 +0000

    Connect Model

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\NewMessageNotification;
 use App\Models\Certificate;
+use App\Models\Payslips;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
