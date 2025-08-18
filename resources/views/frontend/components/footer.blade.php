77b47e376f69c381b82c8fa920a110a9547483be
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-08-18 11:57:35 +0100

    Address Change

--- resources/views/frontend/components/footer.blade.php
+++ resources/views/frontend/components/footer.blade.php
@@ -1,120 +1,126 @@
+<div class="footer-cta5" style="background-image: url({{ asset('frontend/img/bg/footer5-bg.png') }});">
 
-<div class="footer-cta5" style="background-image: url({{ asset('frontend/img/bg/footer5-bg.png')}});">
+
+    <!--===== FOOTER AREA START =======-->
+
+    <div class="footer5 _relative">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="single-footer-items footer-logo-area">
+                        <div class="footer-logo">
+                            <a href=""><img src="{{ asset('frontend/img/logo/routeone_logo_light.svg') }}"
+                                    alt=""></a>
+                        </div>
+                        <div class="space20"></div>
+                        <div class="heading1-w">
+                            <p>We specialize in matching professionals with the right job opportunities for lasting
+                                success.</p>
+                        </div>
+                        <ul class="social-icon">
+                            <li><a href="https://www.facebook.com/profile.php?id=61562057005093" target="_blank"><i
+                                        class="fa-brands fa-facebook"></i></a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg col-md-6 col-12">
+                    <div class="single-footer-items">
+                        <h3>Quick Links</h3>
+
+                        <ul class="menu-list">
+                            <li><a href="#">Terms & Condetions</a></li>
+                            <li><a href="#">Q&A</a></li>
+                            <li><a href="#">Testimonial</a></li>
+                            <li><a href="{{ route('verify') }}">Verify Certificates</a></li>
+                            <li><a href="{{ route('verify.draft') }}">Verify COS Draft</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg col-md-6 col-12">
+                    <div class="single-footer-items pl-5">
+                        <h3>Explore</h3>
+
+                        <ul class="menu-list">
+                            <li><a href="{{ route('/') }}">Home </a></li>
+                            <li><a href="{{ route('about') }}">About Us</a></li>
+                            <li><a href="{{ route('jobs') }}">Jobs</a></li>
+                            <li><a href="{{ route('services') }}">Service</a></li>
+                            <li><a href="{{ route('contact') }}">Contact US</a></li>
+                            <li><a href="{{ route('policy') }}">Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
 
 
-      <!--===== FOOTER AREA START =======-->
+                <div class="col-lg-3 col-md-6 col-12">
+                    <div class="single-footer-items">
+                        <h3>Contact Us</h3>
 
-          <div class="footer5 _relative">
+                        <div class="contact-box">
+                            <div class="icon">
+                                <img src="{{ asset('frontend/img/icons/phone-svgrepo-com.svg') }}" alt="">
+                            </div>
+                            <div class="pera">
+                                <a href="tel:+442031378313">+44 20 313 78 313</a>
+                            </div>
+                        </div>
+
+                        <div class="contact-box">
+                            <div class="icon">
+                                <img src="{{ asset('frontend/img/icons/whatsapp-svgrepo-com.svg') }}" alt="">
+                            </div>
+                            <div class="pera">
+                                <a href="https://wa.me/447361496391">+44 7361 496391</a>
+                            </div>
+                        </div>
+
+                        <div class="contact-box">
+                            <div class="icon">
+                                <img src="{{ asset('frontend/img/icons/envelope-open-svgrepo-com.svg') }}"
+                                    alt="">
+                            </div>
+                            <div class="pera">
+                                <a href="mailto:info@routeonerecruitment.com">info@routeonerecruitment.com</a>
+                            </div>
+                        </div>
+
+                        <div class="contact-box">
+                            <div class="icon">
+                                <img src="{{ asset('frontend/img/icons/location-pin-svgrepo-com.svg') }}"
+                                    alt="">
+                            </div>
+                            <div class="pera">
+                                <a>No 81, Brakems Lane, <br>
+                                    Derby, DE24 0AQ</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="space70"></div>
+        </div>
+
+        <div class="copyright-area _relative">
             <div class="container">
-                 <div class="row">
-                      <div class="col-lg-4 col-md-6 col-12">
-                           <div class="single-footer-items footer-logo-area">
-                                <div class="footer-logo">
-                                  <a href=""><img src="{{ asset('frontend/img/logo/routeone_logo_light.svg') }}" alt=""></a>
-                                </div>
-                                <div class="space20"></div>
-                                <div class="heading1-w">
-                                  <p>We specialize in matching professionals with the right job opportunities for lasting success.</p>
-                                </div>
-                                <ul class="social-icon">
-                                     <li><a href="https://www.facebook.com/profile.php?id=61562057005093" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
-
-                                </ul>
-                           </div>
-                      </div>
-
-                      <div class="col-lg col-md-6 col-12">
-                           <div class="single-footer-items">
-                                <h3>Quick Links</h3>
-
-                                <ul class="menu-list">
-                                     <li><a href="#">Terms & Condetions</a></li>
-                                     <li><a href="#">Q&A</a></li>
-                                     <li><a href="#">Testimonial</a></li>
-                                     <li><a href="{{ route('verify') }}">Verify Certificates</a></li>
-                                     <li><a href="{{ route('verify.draft') }}">Verify COS Draft</a></li>
-                                </ul>
-                           </div>
-                      </div>
-
-                      <div class="col-lg col-md-6 col-12">
-                           <div class="single-footer-items pl-5">
-                                <h3>Explore</h3>
-
-                                <ul class="menu-list">
-                                     <li><a href="{{ route('/') }}">Home </a></li>
-                                     <li><a href="{{ route('about') }}">About Us</a></li>
-                                     <li><a href="{{ route('jobs') }}">Jobs</a></li>
-                                     <li><a href="{{ route('services') }}">Service</a></li>
-                                     <li><a href="{{ route('contact') }}">Contact US</a></li>
-                                     <li><a href="{{ route('policy') }}">Policy</a></li>
-                                </ul>
-                           </div>
-                      </div>
-
-
-                      <div class="col-lg-3 col-md-6 col-12">
-                           <div class="single-footer-items">
-                                <h3>Contact Us</h3>
-
-                                <div class="contact-box">
-                                  <div class="icon">
-                                    <img src="{{ asset('frontend/img/icons/phone-svgrepo-com.svg')}}" alt="">
-                                  </div>
-                                  <div class="pera">
-                                    <a href="tel:+442031378313">+44 20 313 78 313</a>
-                                  </div>
-                                </div>
-
-                                <div class="contact-box">
-                                  <div class="icon">
-                                    <img src="{{ asset('frontend/img/icons/whatsapp-svgrepo-com.svg')}}" alt="">
-                                  </div>
-                                  <div class="pera">
-                                    <a href="https://wa.me/447361496391">+44 7361 496391</a>
-                                  </div>
-                                </div>
-
-                                <div class="contact-box">
-                                  <div class="icon">
-                                    <img src="{{ asset('frontend/img/icons/envelope-open-svgrepo-com.svg')}}" alt="">
-                                  </div>
-                                  <div class="pera">
-                                    <a href="mailto:info@routeonerecruitment.com">info@routeonerecruitment.com</a>
-                                  </div>
-                                </div>
-
-                                <div class="contact-box">
-                                  <div class="icon">
-                                    <img src="{{ asset('frontend/img/icons/location-pin-svgrepo-com.svg')}}" alt="">
-                                  </div>
-                                  <div class="pera">
-                                    <a>464 Alexandra Ave, <br> Rayners Lane, Harrow HA2 9TL</a>
-                                  </div>
-                                </div>
-
-                           </div>
-                      </div>
-
-                 </div>
-
-                 <div class="space70"></div>
-            </div>
-
-            <div class="copyright-area _relative">
-              <div class="container">
                 <div class="row align-items-center">
-                  <div class="col-md-12">
-                       <div class="coppyright">
-                         <p>© Copyright {{ date('Y') }} - Route One Recruitement. All Right Reserved | Developed by <a href="https://codexer.co.uk" target="_blank" rel="Codexer">Codexer</a></p>
-                       </div>
-                  </div>
-             </div>
+                    <div class="col-md-12">
+                        <div class="coppyright">
+                            <p>© Copyright {{ date('Y') }} - Route One Recruitement. All Right Reserved | Developed
+                                by <a href="https://codexer.co.uk" target="_blank" rel="Codexer">Codexer</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-         </div>
+        </div>
 
-      </div>
+    </div>
 
-          <!--===== FOOTER AREA END =======-->
+    <!--===== FOOTER AREA END =======-->
 </div>
--- resources/views/frontend/contact/map.blade.php
+++ resources/views/frontend/contact/map.blade.php
@@ -1,4 +1,8 @@
 <div class="contact-map-page">
     {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2454.5588498102043!2d-0.4875588!3d52.033133899999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4877b35a3f649371%3A0x4b0220de5ae2ccde!2sColston%20Rise%2C%20Ampthill%2C%20Bedford%20MK45%202GN!5e0!3m2!1sen!2suk!4v1727189241616!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> --}}
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.658473764284!2d-0.3731245226354262!3d51.57449417182877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487613368c0d97cb%3A0x1aee3bdad7d6791a!2s464%20Alexandra%20Ave%2C%20Rayners%20Lane%2C%20Harrow%20HA2%209TL!5e0!3m2!1sen!2suk!4v1734535105154!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.658473764284!2d-0.3731245226354262!3d51.57449417182877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487613368c0d97cb%3A0x1aee3bdad7d6791a!2s464%20Alexandra%20Ave%2C%20Rayners%20Lane%2C%20Harrow%20HA2%209TL!5e0!3m2!1sen!2suk!4v1734535105154!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> --}}
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2407.249800509343!2d-1.4437957232963379!3d52.88992517216036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879f05c5142afb1%3A0x8e5906ea3cfa2342!2s81%20Bracken&#39;s%20Ln%2C%20Alvaston%2C%20Derby%20DE24%200AQ!5e0!3m2!1sen!2suk!4v1755514604516!5m2!1sen!2suk"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
